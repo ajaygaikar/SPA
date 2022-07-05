76eb4ff0ea318340725f63e99895c8fa44ebc844
ajaygaikar <ajaygaykar93@gmail.com>
2022-07-05 16:21:58 +0530

    asd
    Main

---
+++ js/app.js
@@ -0,0 +1,32 @@
+(function(){
+    var singlePageApp = angular.module('singlePageApp', ['ngRoute']);
+    
+    singlePageApp.config(function($routeProvider){
+         $routeProvider
+             .when('/', {
+                templateUrl : 'partials/home.html',
+                controller : 'HomeController'
+            })
+            .when('/quiz', {
+                templateUrl : 'partials/quiz.html',
+                controller : 'QuizController'
+            }) 
+            .when('/contact', {
+                templateUrl : 'partials/contact.html',
+                controller : 'ContactController'
+            });
+    });
+    
+    
+    singlePageApp.controller('HomeController',function($scope){
+        $scope.title = "Invoice Script Using PHP MySQL jQuery Ajax And Boostrap - Version 2";
+    });
+    singlePageApp.controller('QuizController',function($scope){
+    	$scope.title = "Responsive PHP Quiz Script";
+    });
+    singlePageApp.controller('ContactController',function($scope){
+    	$scope.title = "Contact";
+    });
+})();
+
+
